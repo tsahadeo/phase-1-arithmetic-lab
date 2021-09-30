e6fade370ddda8e6d0cad7e155de32fcafeaa71a
Travis <tsahadeo@mac.com>
2021-09-30 16:48:48 -0400

    commit tree 8b6ca63af02af6e118c1257bca114ce1c385c14a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) +1;
+
+let num3 = 20
+let num4 = 5
+const mod = (num3 / num4)
+
+const max = Math.max(15, 16, 17, 18, 19, 20)
